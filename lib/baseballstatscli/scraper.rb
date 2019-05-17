d22f9d3598646d2dc71f58ba174624a0dfb24155
jehough <teachlife@outlook.com>
2019-05-17 10:49:50 -0400

    second scraper function

--- lib/baseballstatscli/scraper.rb
+++ lib/baseballstatscli/scraper.rb
@@ -1,9 +1,21 @@
 class Scraper
+    @@site = Nokogiri::HTML(open(https://www.baseball-reference.com/boxes/))
     def get_game_sites (url)
-        site = Nokogiri::HTML(open(url))
-        game_sites = site.css('div.game_summaries').css('td.right.gamelink').map do |games|
+        game_sites = @@site.css('div.game_summaries').css('td.right.gamelink').map do |games|
           url + games.css('a').attribute('href').value
         end
         game_sites
+    end
+    def get_games
+        games = @@site.css('div.game_summaries').css('div.game_summary').map do |game|
+        {
+          :away_team => game.css('table.teams').css('tr').first.css('td').css('a').text.chomp,
+          :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text,
+          :home_team =>game.css('table.teams').css('tr').last.css('td').css('a').text.chomp,
+          :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text
+        }
+        end
+        games
+      end
     end
 end  
