db8ac1ede0d67f3851704122a7a73aa1c066369d
jehough <teachlife@outlook.com>
2019-05-18 12:22:19 -0400

    games

--- lib/baseballstatscli/games.rb
+++ lib/baseballstatscli/games.rb
@@ -2,5 +2,7 @@ class Games
     attr_accessor :away_team, :home_team, :home_score, :away_score, :winning_pitcher, :losing_pitcher
     def initialize
     end
+    def create_from_collection
+    end
 
 end
