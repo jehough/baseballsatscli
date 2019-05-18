0d05e759b7f43cb11a8cb3a4ed9a2a7553a39106
jehough <teachlife@outlook.com>
2019-05-17 21:59:08 -0400

    complete scraper method

--- lib/baseballstatscli/scraper.rb
+++ lib/baseballstatscli/scraper.rb
@@ -1,21 +1,18 @@
 class Scraper
-    @@site = Nokogiri::HTML(open(https://www.baseball-reference.com/boxes/))
-    def get_game_sites (url)
-        game_sites = @@site.css('div.game_summaries').css('td.right.gamelink').map do |games|
-          url + games.css('a').attribute('href').value
-        end
-        game_sites
-    end
-    def get_games
-        games = @@site.css('div.game_summaries').css('div.game_summary').map do |game|
-        {
-          :away_team => game.css('table.teams').css('tr').first.css('td').css('a').text.chomp,
-          :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text,
-          :home_team =>game.css('table.teams').css('tr').last.css('td').css('a').text.chomp,
-          :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text
-        }
-        end
-        games
+  @@site = Nokogiri::HTML(open(https://www.baseball-reference.com/boxes/))
+
+  def get_games
+      games = @@site.css('div.game_summaries').css('div.game_summary').map do |game|
+      {
+        :away_team => game.css('table.teams').css('tr').first.css('td').css('a').text.chomp,
+        :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text,
+        :home_team =>game.css('table.teams').css('tr').last.css('td').css('a').text.chomp,
+        :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text
+        :winning_pitcher => game.css('table')[1].css('tr')[0].css('td')[1].text,
+        :losing_pitcher => game.css('table')[1].css('tr')[1].css('td')[1].text
+      }
       end
+      games
     end
+  end
 end  
