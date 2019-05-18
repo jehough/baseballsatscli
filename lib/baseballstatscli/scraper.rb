2805d657a82be42cf782dd22d61d3ddc11a46708
jehough <teachlife@outlook.com>
2019-05-17 22:04:17 -0400

    minor changes to scraper and games

--- lib/baseballstatscli/scraper.rb
+++ lib/baseballstatscli/scraper.rb
@@ -5,9 +5,9 @@ class Scraper
       games = @@site.css('div.game_summaries').css('div.game_summary').map do |game|
       {
         :away_team => game.css('table.teams').css('tr').first.css('td').css('a').text.chomp,
-        :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text,
+        :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text.to_i,
         :home_team =>game.css('table.teams').css('tr').last.css('td').css('a').text.chomp,
-        :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text
+        :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text.to_i,
         :winning_pitcher => game.css('table')[1].css('tr')[0].css('td')[1].text,
         :losing_pitcher => game.css('table')[1].css('tr')[1].css('td')[1].text
       }
