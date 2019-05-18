08f04b274a52958485843b7546a2346c7565ea4c
jehough <teachlife@outlook.com>
2019-05-18 17:01:47 -0400

    completing the game class

--- lib/baseballstatscli/games.rb
+++ lib/baseballstatscli/games.rb
@@ -1,8 +1,35 @@
-class Games
-    attr_accessor :away_team, :home_team, :home_score, :away_score, :winning_pitcher, :losing_pitcher
-    def initialize
+class Baseballstatscli::Games
+    attr_accessor :game, :away_team, :home_team, :away_score, :home_score, :winner, :winning_pitcher, :loser, :losing_pitcher
+    @@all = []
+    def initialize(team1, team2, game)
+        @game = "#{team1} vs #{team2}"
+        attrs_from_hash(game)
+        find_winner
+        @@all << self
     end
-    def create_from_collection
+    def self.create_from_array(games)
+        games.each do |game|
+        team1 = game[:away_team]
+        team2 = game[:home_team]
+        Game.new(team1, team2, game)
+        end
+    end
+    def attrs_from_hash(game)
+        game.each do |k, v|
+        send("#{k}=", v)
+        end
+    end
+    def find_winner
+        if self.away_score > self.home_score
+        self.winner = self.away_team
+        self.loser = self.home_team
+        else
+        self.winner = self.home_team
+        self.loser = self.away_team
+        end
+    end
+    def self.all
+        @@all
     end
 
 end
--- lib/baseballstatscli/scraper.rb
+++ lib/baseballstatscli/scraper.rb
@@ -1,18 +1,17 @@
-class Scraper
-  @@site = Nokogiri::HTML(open(https://www.baseball-reference.com/boxes/))
+class Baseballstatscli::Scraper
+  @@site = Nokogiri::HTML(open('https://www.baseball-reference.com/boxes/'))
 
   def get_games
-      games = @@site.css('div.game_summaries').css('div.game_summary').map do |game|
-      {
-        :away_team => game.css('table.teams').css('tr').first.css('td').css('a').text.chomp,
-        :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text.to_i,
-        :home_team =>game.css('table.teams').css('tr').last.css('td').css('a').text.chomp,
-        :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text.to_i,
-        :winning_pitcher => game.css('table')[1].css('tr')[0].css('td')[1].text,
-        :losing_pitcher => game.css('table')[1].css('tr')[1].css('td')[1].text
-      }
-      end
-      games
+    games = @@site.css('div.game_summaries').css('div.game_summary').map do |game|
+    {
+      :away_team => game.css('table.teams').css('tr').first.css('td').css('a').text.chomp,
+      :away_score =>game.css('table.teams').css('tr').first.css('td.right').first.text.to_i,
+      :home_team =>game.css('table.teams').css('tr').last.css('td').css('a').text.chomp,
+      :home_score =>game.css('table.teams').css('tr').last.css('td.right').first.text.to_i,
+      :winning_pitcher => game.css('table')[1].css('tr')[0].css('td')[1].text,
+      :losing_pitcher => game.css('table')[1].css('tr')[1].css('td')[1].text
+    }
     end
+    games
   end
 end  
