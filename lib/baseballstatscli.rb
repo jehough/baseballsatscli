b55cbd708b7c43f340f581008c7c3a5c0be73969
jehough <teachlife@outlook.com>
2019-05-17 22:08:15 -0400

    minor changes to structure

--- lib/baseballstatscli.rb
+++ lib/baseballstatscli.rb
@@ -1,5 +1,12 @@
 require "baseballstatscli/version"
+require 'nokogiri'
+require 'open_uri'
 
+require_relative 'baseballstatscli/games.rb'
+require_relative 'baseballstatscli/scraper.rb'
+require_relative 'baseballstatscli/teams.rb'
+require_relative 'baseballstatscli/version.rb'
+require_relative 'baseballstatscli/cli.rb'
 module Baseballstatscli
   class Error < StandardError; end
   # Your code goes here...
